fb94e4605cc1ceffd48131caeb9f597b3f4a2ecc
Dusit Chunviset <dusit_chunviset@cmu.ac.th>
2023-03-29 16:23:58 +0700

    fixed a bug

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) =>{
-   res.render('login'); 
+   res.render('admin-queueing'); 
 });
 
 app.listen("3000", () => {
