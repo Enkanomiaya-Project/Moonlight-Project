15b4a48e2a59434265026b6800934198de84079f
Dusit Chunviset <dusit_chunviset@cmu.ac.th>
2023-04-02 21:02:03 +0700

    Deployment (Fixed #18)

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 exports.connect = () => {
-    mongoose.connect('mongodb://3.214.159.171:27016/', {
+    mongoose.connect('mongodb://3.214.159.171:27017/', {
         auth: {
             username: "Mongo",
             password: "Mongo1234"
